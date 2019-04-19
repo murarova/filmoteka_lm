745e3169a6cd281bd504e17b57316690b6c01750
Stanislav Kaisin <8050361@gmail.com>
2019-04-19 11:28:58 +0300

    added controller for start

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,25 @@
 import '../styles/style.sass';
+import Model from './model';
+import View from './view-test';
+import Controller from './controller';
 
-document.write('Hello world!'); // REMOVE THIS IF DEVELOPMENT WAS STARTED
+const view = new View();
+const model = new Model();
+
+new Controller(model, view);
+
+console.log('model.filmoteka in index.js=', model.filmoteka);
+
+// console.log('model.localStorageAvailable=', model.localStorageAvailable());
+//let test;
+
+//model.favoriteFilms = ['sadfvasdf'];
+// console.log('model.favoriteFilms=', model.favoriteFilms);
+//model.localStorageWrite('favoriteFilms', model.favoriteFilms);
+
+
+//model.localStorageRead('favoriteFilms');
+// console.log('model.localStorageRead(favoriteFilms)=', model.localStorageRead('favoriteFilms'));
+
+//test = model.addFilmToList('favoriteFilms', 'some new film');
+// console.log('model.addFilmToList', test);
