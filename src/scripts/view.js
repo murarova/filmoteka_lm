4798528729f917f25f97790e19d22d60cfd785fb
StanislavKaisin <8050361@gmail.com>
2019-04-20 13:04:38 +0300

    model was formatted

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,6 +1,6 @@
-import {
-    EventEmitter
-} from 'events';
+import { EventEmitter } from "events";
+
+import debounce from "debounce";
 
 // const card = {
 //     "Title": "Guardians of the Galaxy Vol. 2",
@@ -44,274 +44,280 @@ import {
 // };
 
 export default class View extends EventEmitter {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.app = document.querySelector('#app');
-        this.startPage();
-        this.mainPage();
-        // this.makeCardPage(card);
+    this.app = document.querySelector("#app");
+    this.startPage();
+    this.mainPage();
+    // this.makeCardPage(card);
 
-        this.form = this.app.querySelector('.form');
-        // console.log('this.form=', this.form);
-        this.form.addEventListener('submit', this.handleSearch.bind(this));
+    this.input = this.app.querySelector(".input");
+    this.input.addEventListener(
+      "input",
+      debounce(this.onInput.bind(this), 300)
+    );
+  }
 
+  startPage() {
+    this.header(app);
+    this.footer(app);
+  }
+
+  container(root) {
+    const container = document.createElement("div");
+    container.classList.add("container");
+    root.append(container);
+    return container;
+  }
+
+  cardList(root) {
+    const cardList = document.createElement("div");
+    cardList.classList.add("card-list");
+    root.append(cardList);
+    return cardList;
+  }
+
+  header(root) {
+    const header = document.createElement("header");
+    const logo = document.createElement("a");
+    const logoSpanFirst = document.createElement("span");
+    const i = document.createElement("i");
+    const logoSpanSec = document.createElement("span");
+    const menu = document.createElement("ul");
+    const menuItemOne = document.createElement("li");
+    const menuItemTwo = document.createElement("li");
+    const mainPage = document.createElement("a");
+    const myFilmoteka = document.createElement("a");
+
+    header.classList.add("header");
+    logo.classList.add("logo");
+    i.classList.add("logo-icon");
+    menu.classList.add("menu");
+    menuItemOne.classList.add("menu-item");
+    menuItemTwo.classList.add("menu-item");
+    mainPage.classList.add("menu-link");
+    myFilmoteka.classList.add("menu-link");
+
+    mainPage.setAttribute("href", "#");
+    myFilmoteka.setAttribute("href", "#");
+    logo.setAttribute("href", "#");
+
+    logoSpanFirst.textContent = "film";
+    logoSpanSec.textContent = "teka";
+    mainPage.textContent = "Главная страница";
+    myFilmoteka.textContent = "Моя фильмотека";
+
+    root.append(header);
+    header.append(logo);
+    logo.append(logoSpanFirst);
+    logo.append(i);
+    logo.append(logoSpanSec);
+    header.append(menu);
+    menu.append(menuItemOne);
+    menuItemOne.append(mainPage);
+    menu.append(menuItemTwo);
+    menuItemTwo.append(myFilmoteka);
+  }
+
+  footer(root) {
+    const footer = document.createElement("footer");
+    const copy = document.createElement("span");
+    const copyFirst = document.createElement("span");
+    const copySec = document.createElement("span");
+    const i = document.createElement("i");
+    const team = document.createElement("a");
+
+    footer.classList.add("footer");
+    copy.classList.add("copy");
+    team.classList.add("team");
+    i.classList.add("heart-icon");
+
+    copyFirst.textContent = "Made with ";
+    copySec.textContent = " by ";
+    team.textContent = "team one";
+
+    team.setAttribute("href", "#");
+
+    root.append(footer);
+    footer.append(copy);
+    copy.append(copyFirst);
+    copy.append(i);
+    copy.append(copySec);
+    copy.append(team);
+  }
+
+  form(root) {
+    const input = document.createElement("input");
+
+    input.classList.add("input");
+
+    root.append(input);
+  }
+
+  title(root) {
+    const title = document.createElement("h1");
+    title.classList.add("title");
+    title.textContent = "Персональная фильмотека";
+    root.append(title);
+  }
+
+  mainPage() {
+    const container = this.container(this.app);
+    this.title(container);
+    this.form(container);
+    const cardList = this.cardList(container);
+    // this.makeCard(a, cardList);
+    // this.makeCard(a, cardList);
+    // this.makeCard(a, cardList);
+    // this.makeCard(a, cardList);
+    // this.makeCard(a, cardList);
+    // this.makeCard(a, cardList);
+  }
+
+  makeCard(card, root) {
+    
+    const item = document.createElement("div");
+    const title = document.createElement("p");
+    const img = document.createElement("img");
+    const link = document.createElement("a");
+
+    item.classList.add("item");
+    title.classList.add("card-title");
+    img.classList.add("image");
+    link.classList.add("card-link");
+
+    img.setAttribute("src", card.Poster);
+    link.setAttribute("href", "#");
+
+    title.textContent = card.Title;
+
+    item.append(link);
+    link.append(title);
+
+    link.append(img);
+    // root.append(item);
+    return item;
+  }
+
+  makeCardPage(card) {
+    const container = this.container(this.app);
+
+    const shownProp = {
+      Awards: card.Awards,
+      Rating: `${card.Ratings[0].Value}  (${card.imdbVotes} votes)`,
+      Actors: card.Actors,
+      Country: card.Country,
+      Genre: card.Genre,
+      Runtime: card.Runtime
+    };
+
+    const cardPage = document.createElement("div");
+    const cardImage = document.createElement("div");
+    const img = document.createElement("img");
+    const cardInfo = document.createElement("div");
+    const cardTitle = document.createElement("h2");
+    const description = document.createElement("p");
+    const cardYear = document.createElement("span");
+    const cardList = document.createElement("ul");
+    const buttons = document.createElement("div");
+
+    cardPage.classList.add("card-page");
+    cardImage.classList.add("image-wrapper");
+    cardTitle.classList.add("cardPage-title");
+    description.classList.add("desc");
+    cardYear.classList.add("card-year");
+    img.classList.add("card-image");
+    cardInfo.classList.add("card-info");
+    cardList.classList.add("card-info__list");
+    buttons.classList.add("buttons");
+
+    img.setAttribute("src", card.Poster);
+
+    description.textContent = card.Plot;
+    cardTitle.textContent = card.Title;
+    cardYear.textContent = card.Year;
+
+    container.append(cardPage);
+    cardPage.append(cardImage);
+    cardImage.append(img);
+
+    cardPage.append(cardInfo);
+    cardInfo.append(cardTitle);
+    cardInfo.append(description);
+    cardTitle.append(cardYear);
+    cardInfo.append(cardList);
+    cardInfo.append(buttons);
+
+    this.makeButton("Удалить из просмотренных", buttons);
+    this.makeButton("Запланировать просмотр", buttons);
+    this.makeButton("Добавить в избранное", buttons);
+
+    for (const prop in shownProp) {
+      if (shownProp.hasOwnProperty(prop)) {
+        const infoKey = document.createElement("li");
+        const keyValue = document.createElement("span");
+
+        infoKey.textContent = `${prop}: `;
+        keyValue.textContent = shownProp[prop];
+
+        infoKey.classList.add("info-key");
+        keyValue.classList.add("key-value");
+
+        cardList.append(infoKey);
+        infoKey.append(keyValue);
+      }
     }
+  }
 
-    startPage() {
-        this.header(app);
-        this.footer(app);
-    }
+  makeButton(text, root) {
+    const button = document.createElement("button");
+    button.classList.add("button");
+    button.textContent = text;
+    root.append(button);
+  }
 
-    container(root) {
-        const container = document.createElement('div');
-        container.classList.add('container');
-        root.append(container);
-        return container;
-    }
+  //handle search methods
+  clearInput(event) {
+    return (event.target.value = "");
+  }
+  onInput(event) {
+    let inputText = event.target.value;
 
-    cardList(root) {
-        const cardList = document.createElement('div');
-        cardList.classList.add('card-list');
-        root.append(cardList);
-        return cardList;
-    }
+    // console.log('inputText=', inputText);
+    // console.log('this.emit=', this.emit);
 
-    header(root) {
-        const header = document.createElement('header');
-        const logo = document.createElement('a');
-        const logoSpanFirst = document.createElement('span');
-        const i = document.createElement('i');
-        const logoSpanSec = document.createElement('span');
-        const menu = document.createElement('ul');
-        const menuItemOne = document.createElement('li');
-        const menuItemTwo = document.createElement('li');
-        const mainPage = document.createElement('a');
-        const myFilmoteka = document.createElement('a');
+    this.emit("onInputFilmName", inputText);
+  }
 
-        header.classList.add('header');
-        logo.classList.add('logo');
-        i.classList.add('logo-icon');
-        menu.classList.add('menu');
-        menuItemOne.classList.add('menu-item');
-        menuItemTwo.classList.add('menu-item');
-        mainPage.classList.add('menu-link');;
-        myFilmoteka.classList.add('menu-link');
+  updateCardsList(model) {
+    // console.log('model in view', model);
+    console.log('model.queryFilmList=', model.queryFilmList);
+    // console.log('lastQueryTotal=', lastQueryTotal);
+    // console.log('model.queryFilmList=', model.queryFilmList);
 
-        mainPage.setAttribute('href', '#');
-        myFilmoteka.setAttribute('href', '#');
-        logo.setAttribute('href', '#');
+    // const cardList = this.cardList(container);
 
-        logoSpanFirst.textContent = 'film';
-        logoSpanSec.textContent = 'teka';
-        mainPage.textContent = 'Главная страница';
-        myFilmoteka.textContent = 'Моя фильмотека';
+    // console.log("model.queryFilmList=", model.queryFilmList);
+    model.queryFilmList
+    let items = [];
+    model.queryFilmList.forEach(item => {
+    //   console.log("item=", item);
+      console.log("this.makeCard=", this.makeCard);
 
-        root.append(header);
-        header.append(logo);
-        logo.append(logoSpanFirst);
-        logo.append(i);
-        logo.append(logoSpanSec);
-        header.append(menu);
-        menu.append(menuItemOne);
-        menuItemOne.append(mainPage);
-        menu.append(menuItemTwo);
-        menuItemTwo.append(myFilmoteka);
-    }
-
-    footer(root) {
-        const footer = document.createElement('footer');
-        const copy = document.createElement('span');
-        const copyFirst = document.createElement('span');
-        const copySec = document.createElement('span');
-        const i = document.createElement('i');
-        const team = document.createElement('a');
-
-
-        footer.classList.add('footer');
-        copy.classList.add('copy');
-        team.classList.add('team');
-        i.classList.add('heart-icon');
-
-        copyFirst.textContent = 'Made with ';
-        copySec.textContent = ' by ';
-        team.textContent = 'team one';
-
-        team.setAttribute('href', '#');
-
-        root.append(footer);
-        footer.append(copy);
-        copy.append(copyFirst);
-        copy.append(i);
-        copy.append(copySec);
-        copy.append(team);
-    }
-
-    form(root) {
-
-        // const form = document.createElement('form');
-        const input = document.createElement('input');
-
-        // form.classList.add('form');
-        input.classList.add('input');
-
-        root.append(input);
-        // root.append(form);
-    }
-
-    title(root) {
-        const title = document.createElement('h1');
-        title.classList.add('title');
-        title.textContent = 'Персональная фильмотека';
-        root.append(title);
-    }
-
-    mainPage() {
-
-        const container = this.container(this.app);
-        this.title(container);
-        this.form(container);
-        const cardList = this.cardList(container);
-        // this.makeCard(a, cardList);
-        // this.makeCard(a, cardList);
-        // this.makeCard(a, cardList);
-        // this.makeCard(a, cardList);
-        // this.makeCard(a, cardList);
-        // this.makeCard(a, cardList);
-    }
-
-    makeCard(card, root) {
-        const item = document.createElement('div');
-        const title = document.createElement('p');
-        const img = document.createElement('img');
-        const link = document.createElement('a');
-
-        item.classList.add('item');
-        title.classList.add('card-title');
-        img.classList.add('image');
-        link.classList.add('card-link');
-
-        img.setAttribute('src', card.Poster);
-        link.setAttribute('href', '#');
-
-        title.textContent = card.Title;
-
-        item.append(link);
-        link.append(title);
-
-        link.append(img);
-        root.append(item);
-
-        return item;
-    }
-
-    makeCardPage(card) {
-
-        const container = this.container(this.app);
-
-        const shownProp = {
-            Awards: card.Awards,
-            Rating: `${card.Ratings[0].Value}  (${card.imdbVotes} votes)`,
-            Actors: card.Actors,
-            Country: card.Country,
-            Genre: card.Genre,
-            Runtime: card.Runtime
-        };
-
-        const cardPage = document.createElement('div');
-        const cardImage = document.createElement('div');
-        const img = document.createElement('img');
-        const cardInfo = document.createElement('div');
-        const cardTitle = document.createElement('h2');
-        const description = document.createElement('p');
-        const cardYear = document.createElement('span');
-        const cardList = document.createElement('ul');
-        const buttons = document.createElement('div');
-
-        cardPage.classList.add('card-page');
-        cardImage.classList.add('image-wrapper');
-        cardTitle.classList.add('cardPage-title');
-        description.classList.add('desc');
-        cardYear.classList.add('card-year');
-        img.classList.add('card-image');
-        cardInfo.classList.add('card-info');
-        cardList.classList.add('card-info__list');
-        buttons.classList.add('buttons');
-
-        img.setAttribute('src', card.Poster);
-
-        description.textContent = card.Plot;
-        cardTitle.textContent = card.Title;
-        cardYear.textContent = card.Year;
-
-        container.append(cardPage);
-        cardPage.append(cardImage);
-        cardImage.append(img);
-
-        cardPage.append(cardInfo);
-        cardInfo.append(cardTitle);
-        cardInfo.append(description);
-        cardTitle.append(cardYear);
-        cardInfo.append(cardList);
-        cardInfo.append(buttons);
-
-        this.makeButton('Удалить из просмотренных', buttons);
-        this.makeButton('Запланировать просмотр', buttons);
-        this.makeButton('Добавить в избранное', buttons);
-
-        for (const prop in shownProp) {
-
-            if (shownProp.hasOwnProperty(prop)) {
-
-                const infoKey = document.createElement('li');
-                const keyValue = document.createElement('span');
-
-                infoKey.textContent = `${prop}: `;
-                keyValue.textContent = shownProp[prop];
-
-                infoKey.classList.add('info-key');
-                keyValue.classList.add('key-value');
-
-                cardList.append(infoKey);
-                infoKey.append(keyValue);
-            }
-        }
-    }
-
-    makeButton(text, root) {
-        const button = document.createElement('button');
-        button.classList.add('button');
-        button.textContent = text;
-        root.append(button);
-    }
-
-    //handle search methods
-    clearInput(event) {
-        return event.target[0].value = '';
-    }
-
-    handleSearch(event) {
-        event.preventDefault();
-        let input = event.target[0];
-        let inputFilmName = input.value;
-        this.emit('onInputFilmName', inputFilmName);
-        this.clearInput(event);
-        // console.log('inputFilmName=', inputFilmName);
-    }
-
-    updateCardsList(model) {
-        // console.log('model in view', model);
-        // console.log('model.queryFilmList=', model.queryFilmList);
-        // console.log('lastQueryTotal=', lastQueryTotal);
-        // console.log('model.queryFilmList=', model.queryFilmList);
-        const cardList = this.cardList(container);
-        console.log('model.queryFilmList=', model.queryFilmList);
-        model.queryFilmList.forEach(
-            item=>{
-                console.log('item=', item);
-                // makeCard(item, cardList);
-            }
-        )
-    }
-
+      let newCard = this.makeCard(item, cardList);
+      console.log("newCard=", newCard);
+      items.push(newCard);
+    });
+    console.log("items=", items);
+  }
 }
+/*
+title.textContent = card.Title;
+
+    item.append(link);
+    link.append(title);
+
+    link.append(img);
+    root.append(item);
+*/
